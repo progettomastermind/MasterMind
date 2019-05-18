cbd61d6319de5d532ccf0491a8cd2f61fb6cf410
mgostIH <dennizmodesti@gmail.com>
2019-05-18 17:25:36 +0200

    Aggiunte documentazioni a ConfigHandler

--- ConfigHandler.java
+++ ConfigHandler.java
@@ -4,12 +4,26 @@ import java.io.FileOutputStream;
 import java.io.Writer;
 import java.io.OutputStreamWriter;
 
+/**
+ * Classe per salvare e caricare le configurazioni di gioco tramite file
+ */
 class ConfigHandler{
+    /**
+     * Handle utilizzato per leggere e scrivere su file
+     */
     private File config;
+    
+    /**
+     * Salva e legge sul file config.txt di default
+     */
     ConfigHandler(){
         config = new File("config.txt");
     }
 
+    /**
+     * Legge la configurazione del gioco presente nel file selezionato durante la costruzione
+     * @return Configurazione del gioco letta, il riferimento è nullo se ci sono stati errori
+     */
     public GameConfig readConfigs(){
         try{
             Scanner reader = new Scanner(config);
@@ -24,7 +38,12 @@ class ConfigHandler{
             return null;
         }
     }
-
+    
+    /**
+     * Scrive la configurazione del gioco sul file selezionato durante la costruzione
+     * @param data Configurazione da scrivere
+     * @return `true` se il file è stato sovrascritto con successo con la configurazione da salvare, altrimenti `false`
+     */
     public boolean writeConfigs(GameConfig data){
         if (!config.canWrite()) return false;
         try{
