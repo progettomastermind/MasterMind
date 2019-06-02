7b1d52e40b63aa86ff75df4c7e01f2373c68a1df
mgostIH <dennizmodesti@gmail.com>
2019-06-02 19:09:54 +0200

    Aggiunte docstring per funzioni in randomness

--- Randomness.java
+++ Randomness.java
@@ -1,7 +1,12 @@
 import java.util.Random;
 
 public class Randomness{
-
+    /**
+     * Crea un array casuale (con possibili ripetizioni) di colori 
+     * @param length Lunghezza array
+     * @param color_amount Quantità di colori diversi
+     * @return Array di colori casuale
+     */
     public static int[] getRandomSequence(int length, int color_amount){
         Random random = new Random();
         int[] seq = new int[length];
@@ -11,13 +16,18 @@ public class Randomness{
         return seq;
     }
 
+    /**
+     * Crea un array mescolata (senza ripetizioni) lunga `color_amount`
+     * @param color_amount
+     * @return Array di colori mescolati
+     */
     public static int[] getShuffledSequence(int color_amount){
         Random random = new Random();
         int[] rainbow = new int[color_amount];
         int c;
         int tmp;
 
-        for(int i=0; i<color_amount; i++)
+        for(int i=0; i<color_amount; i++)   // Viene riempita un array di colori in ordine
             rainbow[i]=i;
 
         for(int i=0; i<length; i++){
