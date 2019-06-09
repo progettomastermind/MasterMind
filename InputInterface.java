fd11fc5f80271ab95947d9f9926520cad7cd3f04
mgostIH <dennizmodesti@gmail.com>
2019-06-09 16:12:58 +0200

    Aggiunto metodo per leggere riga di colori

---
+++ InputInterface.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class InputInterface {
+    // This needs to be static because closing a scanner will also close the input
+    // stream
+    // This means you cannot close it twice or you'll run into a
+    // NoSuchElementException
+    private static Scanner keyboard = new Scanner(System.in);
+
+    public static int[] ReadColors(int amount) {
+        int[] colors = new int[amount];
+        for (int i = 0; i < amount; i++)
+            colors[i] = keyboard.nextInt();
+        return colors;
+    }
+}
--- Main.java
+++
@@ -1,9 +0,0 @@
-class Main{
-    public static void main(String[] Args){
-        ConfigHandler r = new ConfigHandler();
-        GameConfig data = r.readConfigs();
-        System.out.println(data);
-        data.colors = 4;
-        System.out.println(r.writeConfigs(data));
-    }
-}
