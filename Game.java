c9a7b2683252ed231880477fea6cc6015b6fc033
mgostIH <dennizmodesti@gmail.com>
2019-07-12 16:27:29 +0200

    Aggiornata immagine di esempio e aggiunto un newline dopo la breve introduzione

--- Game.java
+++ Game.java
@@ -24,7 +24,7 @@ public class Game{
         System.out.println("Il gioco inizia! Hai " + data.attempts + " tentativi.");
         System.out.println("Scrivi " + data.length + " colori separati da spazio, ovvero numeri che vanno da 0 a " + (data.colors - 1) + ".");
         System.out.println("Scrivendo numeri >= " + data.colors + " questi non verranno considerati.");
-        System.out.println("I colori " + (!data.can_repeat_colors ? "non " : "") + "possono essere ripetuti.");    // ? : viene detto operatore ternario (Spiegazione a parte)
+        System.out.println("I colori " + (!data.can_repeat_colors ? "non " : "") + "possono essere ripetuti.\n");    // ? : viene detto operatore ternario (Spiegazione a parte)
         boolean is_completed = false;   // Serve un booleano da inizializzare per capire se a fine ciclo il gioco sia vinto o perso
         for(int attempts = 0; attempts < data.attempts; attempts++){
             int[] colors = InputInterface.readColors(data.length);  // Leggiamo i colori dall'utente
